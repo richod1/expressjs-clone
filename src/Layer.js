e97e3ba26f6c0f2431b8705a6ba0a9248d1b63b5
richod1 <frimpsup@gmail.com>
2024-04-17 07:34:42 +0000

    first commit

---
+++ src/Layer.js
@@ -0,0 +1,25 @@
+function Layer(options,path,fn){
+    if(!(this instanceof Layer)){
+        return new Layer(path,options.fn);
+    }
+    this.handle=fn;
+    this.name=fn.name|| `<anonymous>`;
+    this.params=undefined;
+    this.path=undefined;
+
+}
+
+Layer.prototype.match=function match(path){
+    return this.route.path===path;
+}
+
+Layer.prototype.handle_request=function handle(req,res,next){
+    let fn=this.handle;
+    try{
+        fn(req,res,next)
+    }catch(error){
+        console.log(error)
+        return new Error("failed to handle request")
+    }
+
+}
